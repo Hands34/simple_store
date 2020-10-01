edc7a8b2b2caa6db91628ec98586018918209255
Mitchell Cook <m.cook2434@gmail.com>
2020-10-01 16:29:14 -0500

    deleted view View that was created in error, added show View

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # get 'products/index'
-  resources :products, only: %i[index view]
-  # get 'products/view'
+  resources :products, only: %i[index show]
+  # get 'products/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
