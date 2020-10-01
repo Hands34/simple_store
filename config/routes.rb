e0a273a0f6341c064a205eba1f325ce534d5c43a
Mitchell Cook <m.cook2434@gmail.com>
2020-10-01 16:26:23 -0500

    added instance variable for products, modified index view, changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/view'
+  # get 'products/index'
+  resources :products, only: %i[index view]
+  # get 'products/view'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
