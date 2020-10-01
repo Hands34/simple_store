7a55f6dbafb29b663d0d940bb311180b76ffa87b
Mitchell Cook <m.cook2434@gmail.com>
2020-10-01 16:12:16 -0500

    modified seed file to use Faker to create 676 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+Product.delete_all
+
+all_products = Product.all
+puts "There are #{all_products.count} products in this database"
+newest_prod = all_products.last
+
+new_products = []
+
+new_products.insert(
+  676.times do
+    Product.create(
+      title:          Faker::Commerce.product_name,
+      price:          Faker::Commerce.price,
+      stock_quantity: Faker::Number.number
+    )
+  end
+)
+puts "There are #{all_products.count} products in this database"
